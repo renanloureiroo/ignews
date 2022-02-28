3120ae1cdc07c5f2486d05732727d79a75ea4c45
Renan Loureiro <renanpl47@gmail.com>
2022-02-28 17:06:50 -0300

    feat: view post

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,29 @@
 import Image from "next/image"
+
+import { useRouter } from "next/router"
+
 import Logo from "../../public/images/logo.svg"
+import { ActiveLink } from "../ActiveLink"
 import { SignInButton } from "../SignInButton"
 
 import styles from "./styles.module.scss"
 
 export const Header = () => {
+  const { asPath } = useRouter()
+  console.log(asPath)
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src={Logo} alt="ig.news" />
 
         <nav>
-          <a href="" className={styles.active}>
-            Home
-          </a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,26 @@
+import { GetStaticProps } from "next"
 import Head from "next/head"
-import styles from "./styles.module.scss"
 
-export default function Posts() {
+import Prismic from "@prismicio/client"
+import { RichText } from "prismic-dom"
+
+import { getPrismicClient } from "../../service/prismic"
+import styles from "./styles.module.scss"
+import Link from "next/link"
+
+type Post = {
+  slug: string
+  title: string
+  excerpt: string
+  updatedAt: string
+}
+
+interface Props {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: Props) {
+  console.log(posts)
   return (
     <>
       <Head>
@@ -10,32 +29,50 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a Monorepo to manage
-              multiple packages with a shared build, test, and release process.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a Monorepo to manage
-              multiple packages with a shared build, test, and release process.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a Monorepo to manage
-              multiple packages with a shared build, test, and release process.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+  const response = await prismic.query<any>(
+    [Prismic.predicates.at("document.type", "post")],
+    {
+      fetch: ["post.title", "post.content"],
+      pageSize: 100,
+    }
+  )
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    }
+  })
+  return {
+    props: { posts },
+    revalidate: 60 * 60, // 1 hour
+  }
 }
