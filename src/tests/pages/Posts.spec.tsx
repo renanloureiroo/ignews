684eef5c30a6705c8b703d98d3f4add40a1a08b7
Renan Pereira Loureiro <renanpl47@gmail.com>
2022-03-31 14:38:13 -0300

    test: Posts page

---
+++ src/tests/pages/Posts.spec.tsx
@@ -0,0 +1,59 @@
+import { render, screen } from "@testing-library/react"
+import { mocked } from "jest-mock"
+import Posts, { getStaticProps } from "../../pages/posts"
+import { getPrismicClient } from "../../services/prismic"
+
+const posts = [
+  {
+    slug: "my-new-post",
+    title: "My New Post",
+    excerpt: "Post excerpt",
+    updatedAt: "31 de março",
+  },
+]
+
+jest.mock("../../services/prismic")
+
+describe("Posts page", () => {
+  it("renders correctly", () => {
+    render(<Posts posts={posts} />)
+
+    expect(screen.getByText("My New Post")).toBeInTheDocument()
+  })
+
+  it("loads initial data", async () => {
+    const getPrismicClientMocked = mocked(getPrismicClient)
+
+    getPrismicClientMocked.mockReturnValueOnce({
+      query: jest.fn().mockResolvedValueOnce({
+        results: [
+          {
+            uid: "my-new-post",
+            data: {
+              title: [{ type: "heading", text: "My new post" }],
+              content: [{ type: "paragraph", text: "Content my post" }],
+            },
+            last_publication_date: "04-01-2022",
+          },
+        ],
+      }),
+    } as any)
+
+    const response = await getStaticProps({})
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          posts: [
+            {
+              slug: "my-new-post",
+              title: "My new post",
+              excerpt: "Content my post",
+              updatedAt: "01 de abril de 2022",
+            },
+          ],
+        },
+      })
+    )
+  })
+})
